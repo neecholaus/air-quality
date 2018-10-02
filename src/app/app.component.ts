b4dc7921fbbe060444d4760c96aa3e94926a5c2a
Nicholas Neuman <neecholaus@gmail.com>
2018-10-01 20:00:53 -0700

    Search function is now bound with client input.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,15 @@ import * as toastr from 'toastr';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    API_URL = 'http://api.waqi.info/feed/buffalo/?token=demo';
+    constructor(private http: HttpClient) { }
+
+    search = 'here';
+
+    token = '44ec0d6619f00dcd5f84f6293c77529e5ad94b69';
+
+    url = 'http://api.waqi.info/feed/' + this.search + '/?token=' + this.token;
 
     response: Response;
-
-    constructor(private http: HttpClient) { }
 
     ngOnInit() {
         toastr.options = {
@@ -35,9 +39,10 @@ export class AppComponent {
         };
     }
 
-    getResponse(): void {
-        this.http.get(this.API_URL).pipe().subscribe((response) => {
+    getResponse(): void {        
+        this.http.get(this.url).pipe().subscribe((response) => {
             this.response = <Response>response;
+            console.log(response);
 
             if (this.response.status === 'ok') {
                 toastr.success('Location found.', 'Success!');
